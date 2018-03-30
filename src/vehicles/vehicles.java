459ec21c1e4fca152b75e54cbc51606cc3667692
John <John@LAPTOP-1NIEGMFE>
2018-03-30 16:37:56 -0700

    Add gallonsOfGas and mpg + getters and setters

---
+++ src/vehicles/vehicles.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehicles;
+
+/**
+ *
+ * @author John
+ */
+public class vehicles {
+     private double gallonsOfGas;
+     private int mpg;
+
+    /**
+     * @return the gallonsOfGas
+     */
+    public double getGallonsOfGas() {
+        return gallonsOfGas;
+    }
+
+    /**
+     * @param gallonsOfGas the gallonsOfGas to set
+     */
+    public void setGallonsOfGas(double gallonsOfGas) {
+        this.gallonsOfGas = gallonsOfGas;
+    }
+
+    /**
+     * @return the mpg
+     */
+    public int getMpg() {
+        return mpg;
+    }
+
+    /**
+     * @param mpg the mpg to set
+     */
+    public void setMpg(int mpg) {
+        this.mpg = mpg;
+    }
+     
+    
+    
+    
+    
+}
